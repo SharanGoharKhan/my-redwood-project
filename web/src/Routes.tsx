fe0a663dedd861031af10e6c0eeb3e17213be2f1
Sharan <sharan.gohar@gmail.com>
2022-09-24 15:39:59 +0500

    fixed routes bug in posts

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -2,12 +2,20 @@ import { Router, Route, Set } from '@redwoodjs/router'
 
 import BlogLayout from 'src/layouts/BlogLayout'
 
+import PostsLayout from './layouts/PostsLayout/PostsLayout'
+
 const Routes = () => {
   return (
     <Router>
       <Set wrap={BlogLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
+      </Set>
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
