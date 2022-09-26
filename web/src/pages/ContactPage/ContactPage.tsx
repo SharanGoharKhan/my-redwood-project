704ec1fc97e6609ae2ff8449b5b6c4b0f98ad3b6
sharan <sharan.gohar@gmail.com>
2022-09-26 18:14:24 +0500

    added contact form

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,14 +1,71 @@
-import { Form, TextField } from '@redwoodjs/forms'
-import { MetaTags } from '@redwoodjs/web'
+import {
+  FieldError,
+  Form,
+  FormError,
+  Label,
+  Submit,
+  TextAreaField,
+  TextField,
+  useForm,
+} from '@redwoodjs/forms'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/dist/toast'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
+  const formMethods = useForm()
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your message.')
+      formMethods.reset()
+    }
+  })
+  const onSubmit = (data) => {
+    console.log(data)
+    create({ variables: { input: data } })
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
-      <Form>
-        <TextField name="name" />
-        <TextField name="email" />
-        <TextField name="message" />
+      <Toaster />
+      <Form onSubmit={onSubmit} formMethods={formMethods} error={error}>
+        <FormError error={error} wrapperClassName='form-error' />
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField name="name" validation={{ required: true }} />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField name="message" validation={{ required: true }} />
+        <FieldError name="message" className="error" />
+
+        <Submit disabled={loading}>Send Message</Submit>
       </Form>
     </>
   )
